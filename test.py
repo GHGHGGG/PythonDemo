4f6cfcd03f7f0552de8c9760ac6c1784113ea085
GHGHGGG <51103874+GHGHGGG@users.noreply.github.com>
2019-06-04 10:36:41 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,3 +2,7 @@ import machine
 import dht
 print("hsgfjbdSFJHSDFHFJHGDSFHjg")
 print("JSgfdjhsadgjbdjhdsg")
+print("孑然一身孑然一身孑然一身")
+print("孑然一身孑然一身孑然一身")
+print("孑然一身孑然一身孑然一身")
+print("孑然一身孑然一身孑然一身")
